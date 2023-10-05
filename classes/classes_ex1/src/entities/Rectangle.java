2e474a8cfa26c57b419a4df23c5ba17ddad898d9
fernandagoncalvess <goncalves.de.fernanda@gmail.com>
2023-10-04 22:07:44 -0300

    atualizando sobre classes

---
+++ classes/classes_ex1/src/entities/Rectangle.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Rectangle {
+
+	public Double width;
+	public Double height;
+	
+	public double area(){
+		return height * width;
+		
+	}
+	public double perimeter(){
+		return 2 * (width*height);
+	}
+	public double diagonal(){
+		return Math.sqrt(width * width + height * height);
+	}
+	
+}
